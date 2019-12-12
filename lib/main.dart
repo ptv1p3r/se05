e2813da99ab183aa5bd7e5edff015c19286ce1c8
Pedro Roldan <pedro.roldang@mail.com>
2019-12-12 15:48:33 +0000

    name changed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ISMAT SE Android Client',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
