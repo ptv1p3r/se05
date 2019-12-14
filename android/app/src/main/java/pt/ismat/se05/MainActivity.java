cabbbeb2c897b227a3b4b8523476e2c7100fbfb7
Pedro Roldan <pedro.roldan@gmail.com>
2019-12-14 17:04:46 +0000

    datetime added to data point storage

--- android/app/src/main/java/pt/ismat/se05/MainActivity.java
+++ android/app/src/main/java/pt/ismat/se05/MainActivity.java
@@ -18,6 +18,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
@@ -26,79 +27,75 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity implements LocationListener {
 
-  public final int REQUEST_CODE_INTERNET = 103;
-  public final int REQUEST_CODE_ACCESS_FINE_LOCATION = 100;
-  public final int REQUEST_CODE_ACCESS_COARSE_LOCATION = 101;
-  public final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 102;
+    public final int REQUEST_CODE_INTERNET = 103;
+    public final int REQUEST_CODE_ACCESS_FINE_LOCATION = 100;
+    public final int REQUEST_CODE_ACCESS_COARSE_LOCATION = 101;
+    public final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 102;
 
-  private Intent forService;
+    private Intent forService;
 
-  private LocationManager locationManager;
-  private String provider;
+    private LocationManager locationManager;
+    private String provider;
 
-  SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
-  Date now = new Date();
-  String fileName = formatter.format(now) + ".csv";
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
 
 //    // Permission check and request
 //    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 //        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
 //    }
 
-    forService = new Intent(MainActivity.this,MyService.class);
+        forService = new Intent(MainActivity.this,MyService.class);
 
-    new MethodChannel(getFlutterView(),"pt.ismat.se05.messages")
-            .setMethodCallHandler(new MethodChannel.MethodCallHandler() {
-              @Override
-              public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
-                if(methodCall.method.equals("startService")){
-                  startService();
-                  startDataTracking();
-                  result.success("Service Started");
-                }
-              }
-            });
-  }
+        new MethodChannel(getFlutterView(),"pt.ismat.se05.messages")
+                .setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                        if(methodCall.method.equals("startService")){
+                            startService();
+                            startDataTracking();
+                            result.success("Service Started");
+                        }
+                    }
+                });
+    }
 
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    stopService(forService);
-  }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(forService);
+    }
 
-  /* Request updates at startup */
-  @Override
-  protected void onResume() {
-    super.onResume();
-    System.out.println("onResume");
-  }
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume");
+    }
 
-  /* Remove the locationlistener updates when Activity is paused */
+    /* Remove the locationlistener updates when Activity is paused */
 //  @Override
 //  protected void onPause() {
 //    super.onPause();
 //    locationManager.removeUpdates(this);
 //  }
 
-  private void startDataTracking(){
+    private void startDataTracking(){
 
-    // Permission check and request
-    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE_ACCESS_FINE_LOCATION);
-      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE_ACCESS_COARSE_LOCATION);
-    }
+        // Permission check and request
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE_ACCESS_FINE_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE_ACCESS_COARSE_LOCATION);
+        }
 
-    // Get the location manager
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    Criteria criteria = new Criteria();
-    provider = locationManager.getBestProvider(criteria, true);
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
 
-    locationManager.requestLocationUpdates(provider, 0, 0, this);
+        locationManager.requestLocationUpdates(provider, 0, 0, this);
 //    Location location = locationManager.getLastKnownLocation(provider);
 //
 //    // Initialize the location fields
@@ -109,67 +106,70 @@ public class MainActivity extends FlutterActivity implements LocationListener {
 //      System.out.println("Location not available");
 //    }
 
-  }
-
-  private void startService(){
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-      startForegroundService(forService);
-    } else {
-      startService(forService);
-    }
-  }
-
-  private void writeDataToFile(double _latitude, double _longitude, double _altitude, double _speed, double _bearing){
-
-    // Permission check and request
-    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
     }
 
-    try
-    {
-      File root = new File(Environment.getExternalStorageDirectory()+ File.separator + "Trajetorias");
-
-      if (!root.exists())
-      {
-        root.mkdirs();
-      }
-
-      File dataFile = new File(root, fileName);
-
-      FileWriter writer = new FileWriter(dataFile,true);
-      writer.append(_latitude + "," + _longitude + "," + _altitude + "," + _speed + "," + _bearing + "\n");
-      writer.flush();
-      writer.close();
+    private void startService(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            startForegroundService(forService);
+        } else {
+            startService(forService);
+        }
     }
-    catch(IOException e)
-    {
-      e.printStackTrace();
-    }
-  }
 
-  @Override
-  public void onLocationChanged(Location location) {
+    private void writeDataToFile(double _latitude, double _longitude, double _altitude, double _speed, double _bearing){
+
+        // Permission check and request
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
+        }
+
+        try
+        {
+            File root = new File(Environment.getExternalStorageDirectory()+ File.separator + "Trajetorias");
+            if (!root.exists())
+            {
+                root.mkdirs();
+            }
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+            Date now = new Date();
+            String fileName = formatter.format(now) + ".csv";
+            File dataFile = new File(root, fileName);
+
+            String currentDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+            FileWriter writer = new FileWriter(dataFile,true);
+            writer.append(_latitude + "," + _longitude + "," + _altitude + "," + _speed + "," + _bearing + "," + currentDateTime + "\n");
+            writer.flush();
+            writer.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
 //    System.out.println("Coordenadas lat: " + location.getLatitude() + " lng: " + location.getLongitude());
 
-    if (location != null){
-      writeDataToFile(location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getSpeed(), location.getBearing());
+        if (location != null){
+            writeDataToFile(location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getSpeed(), location.getBearing());
+        }
+
     }
 
-  }
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        System.out.println("onStatusChanged: " + s);
+    }
 
-  @Override
-  public void onStatusChanged(String s, int i, Bundle bundle) {
-    System.out.println("onStatusChanged: " + s);
-  }
+    @Override
+    public void onProviderEnabled(String s) {
+        System.out.println("onProviderEnabled: " + s);
+    }
 
-  @Override
-  public void onProviderEnabled(String s) {
-    System.out.println("onProviderEnabled: " + s);
-  }
-
-  @Override
-  public void onProviderDisabled(String s) {
-    System.out.println("onProviderDisabled: " + s);
-  }
+    @Override
+    public void onProviderDisabled(String s) {
+        System.out.println("onProviderDisabled: " + s);
+    }
 }
